7cc3bf8e64ab2f743cd26108a928bbc61ea23be1
Your Name <ciftci75can@gmail.com>
2024-05-16 19:11:12 +0300

    .

--- Backend/routers/auth.router.js
+++ Backend/routers/auth.router.js
@@ -53,4 +53,13 @@ router.post("/login", async (req, res)=> {
     }
 })
 
+router.get("/getAll", async (req, res)=> {
+    try {
+        const users = await User.find().sort({name: 1});
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
 module.exports = router;
